73260cbd1a4795119c0a7cc2882654ddbd2ff4fb
admin <admin@DESKTOP-LLVLRIF>
2022-09-11 19:25:32 +0530

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expense_tracker.Models
+{
+    
+     public class Transaction
+    {
+        [Key]
+        public int TransationId { get; set; }
+
+        public int CategotyId { get; set; }
+        public Category Category { get; set; }  
+        
+        public int Amount { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string? Note  { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+
+    }
+}
